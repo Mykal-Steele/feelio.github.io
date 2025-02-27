314493d94a4a04c6344a729255a1a64c2774e084
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-02-27 18:00:34 +0700

    render setup

--- feelio/vite.config.js
+++ feelio/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/feelio.github.io/",
   plugins: [react()],
-  base: "/feelio/",
+  base: "/feelio.github.io/",
 });
