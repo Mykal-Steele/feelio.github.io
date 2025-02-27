a5bb6efa079e6b628d37c78234a51abf60578890
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-02-27 19:20:34 +0700

    g

--- feelio/vite.config.js
+++ feelio/vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/feelio.github.io/",
-=======
->>>>>>> parent of af20763 (w)
 });
