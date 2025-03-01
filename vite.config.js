fe53c3bdd43ba7cbd77db99cdd8b901de1020fe8
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-03-01 23:54:53 +0700

    upd

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default defineConfig({
-  base: "/feelio.github.io/",
+  base: "/",
   plugins: [react()],
   define: {
     "process.env": Object.fromEntries(
